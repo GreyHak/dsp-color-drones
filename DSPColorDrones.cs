61b08209eb857fea303e2ec061726b3f37f1a769
GreyHak <479851+GreyHak@users.noreply.github.com>
2021-03-19 15:07:31 -0700

    Base mod without any functionality

---
+++ DSPColorDrones.cs
@@ -0,0 +1,46 @@
+﻿//
+// Copyright (c) 2021, Aaron Shumate
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE.txt file in the root directory of this source tree. 
+//
+// Dyson Sphere Program is developed by Youthcat Studio and published by Gamera Game.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace DSPColorDrones
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    [BepInProcess("DSPGAME.exe")]
+    public class DSPColorDrones : BaseUnityPlugin
+    {
+        public const string pluginGuid = "greyhak.dysonsphereprogram.:::NAME:::";
+        public const string pluginName = "DSP :::NAME:::";
+        public const string pluginVersion = "1.0.0";
+        new internal static ManualLogSource Logger;
+        new internal static BepInEx.Configuration.ConfigFile Config;
+        Harmony harmony;
+
+        public void Awake()
+        {
+            Logger = base.Logger;  // "C:\Program Files (x86)\Steam\steamapps\common\Dyson Sphere Program\BepInEx\LogOutput.log"
+            Config = base.Config;  // "C:\Program Files (x86)\Steam\steamapps\common\Dyson Sphere Program\BepInEx\config\"
+
+            harmony = new Harmony(pluginGuid);
+            harmony.PatchAll(typeof(DSP:::NAME:::));
+        }
+
+        [HarmonyPfix, HarmonyPatch(typeof(CLASS), "FUNCTION")]
+        public static void CLASS_FUNCTION_Pfix()
+        {
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿//
+// Copyright (c) 2021, Aaron Shumate
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE.txt file in the root directory of this source tree. 
+//
+// Dyson Sphere Program is developed by Youthcat Studio and published by Gamera Game.
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DSP Color Drones")]
+[assembly: AssemblyDescription("Mod to change drone colors for Dyson Sphere Program")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DSP Color Drones")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0a68b2c1-2fbd-4ae2-afaf-fc0b09dfda00")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
